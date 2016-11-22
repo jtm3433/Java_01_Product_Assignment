059b22597074a6425a76d35e50d09cce3c119179
jtm3433 <john.mitch96@utexas.edu>
2016-11-22 14:58:42 -0600

    completed assignment locally

--- Product_Assignment_jtm3433.java
+++ Product_Assignment_jtm3433.java
@@ -3,4 +3,7 @@
 // It should be named Product_<<eid>>.java   (Product_jr007.java)
 // It should have a private sku instance variable containing your eid.
 
-	
+class Product_Assignment_jtm3433.java {
+   private String eid  "jtm3433";
+
+}
